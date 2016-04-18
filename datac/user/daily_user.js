d91b86cbd44c3e6e088a7e9b9e1b2c641f1cc1ac
BingB <lyb0307@qq.com>
2016-04-18 15:53:48 +0800

    fill time for user

--- datac/user/daily_user.js
+++ datac/user/daily_user.js
@@ -31,12 +31,14 @@ MongoClient.connect(url).then(function (db) {
 
     var cursor = collection.find();
     var ids = [];
-    var promises = [];
+    var map = {};
     new Promise(function (resolve, reject) {
+        var promises = [];
         cursor.forEach(function (doc) {
             promises.push(publicCollection.findOne({_id: doc._id}, {fields: {user: 1}}).then(function (doc) {
                 if (doc && doc.user) {
                     ids.push(doc.user);
+                    map[doc.user] = doc._id;
                 }
             }));
         }, function (err) {
@@ -55,13 +57,35 @@ MongoClient.connect(url).then(function (db) {
         var sql = "SELECT ?? FROM user_info WHERE id IN (?)";
         var columns = ['id', 'birthday', 'city', 'gender', 'nickname', 'identity', 'university', 'profession'];
         var values = [columns, ids];
-        connection.query(sql, values, function (err, results) {
-            if (err) {
-                console.log('mysql query error:', err);
-            } else {
-                console.log('result', results);
-            }
+        return new Promise(function (resolve, reject) {
+            connection.query(sql, values, function (err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            })
+        });
+    }).then(function (results) {
+        console.log('users:', results.length);
+        var promises = [];
+        results.forEach(function (user) {
+            promises.push(collection.findOne({_id: map[user.id]}).then(function (doc) {
+                var activeDate = new Date(doc.time);
+                var firstDate = new Date(doc.first_time);
+                if (activeDate && firstDate && ((activeDate - firstDate) > (1000 * 60 * 60 * 24 * 7))) {
+                    user.from_time = doc.first_time;
+                } else {
+                    user.from_time = doc.time;
+                }
+                return user;
+            }))
         })
+        return promises;
+    }).then(function (promises) {
+        return Promise.all(promises);
+    }).then(function (users) {
+        console.log(users);
     }).catch(function (err) {
         console.log('err:', err);
         db.close();
